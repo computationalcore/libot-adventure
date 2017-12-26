28ddc292369697060b851cff1a7e8dbc4096abaa
Vin Busquet <computationalcore@gmail.com>
2017-12-26 06:48:01 -0200

    feat: Add main menu

---
+++ Assets/Scripts/UIButtonOpenGithub.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+
+public class UIButtonOpenGithub : MonoBehaviour {
+
+	public void openGithub() {
+		//Open the project github page
+		Application.OpenURL("https://github.com/computationalcore/sphero-adventure/");
+	}
+
+}
