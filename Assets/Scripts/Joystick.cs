5b06469526e9fca94c2c4fcb3c7f0a3b1ca378c1
Vin Busquet <computationalcore@gmail.com>
2018-01-01 14:38:35 -0200

    feat: Add mobile touch fixed controller

---
+++ Assets/Scripts/Joystick.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class Joystick : MonoBehaviour, IDragHandler, IPointerUpHandler, IPointerDownHandler
+{
+    [Header("Options")]
+    [Range(0f, 2f)] public float handleLimit = 1f;
+
+    [HideInInspector] public Vector2 inputVector = Vector2.zero;
+
+    [Header("Components")]
+    public RectTransform background;
+    public RectTransform handle;
+
+    public float Horizontal { get { return inputVector.x; } }
+    public float Vertical { get { return inputVector.y; } }
+
+    public virtual void OnDrag(PointerEventData eventData)
+    {
+
+    }
+
+    public virtual void OnPointerDown(PointerEventData eventData)
+    {
+
+    }
+
+    public virtual void OnPointerUp(PointerEventData eventData)
+    {
+
+    }
+}
